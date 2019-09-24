49f0f62f42436cc797384381358c7f709dc48eed
idmyn <hello@davidmyno.rs>
2019-09-24 16:37:42 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(new_information)
+    knowledge << new_information
+  end
 end
