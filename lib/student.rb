16c6ee5e7cdb3c3ca375b01176fe42b06657c164
Marco-FI-SEI <mmc90@hotmail.co.uk>
2019-09-28 16:11:15 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
 
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(subject)
+        self.knowledge << subject
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
